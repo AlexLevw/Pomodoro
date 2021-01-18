4c65b2d3ab2064668a5b39bc93463ec6800abaff
Aleksandr <foxi4554@gmail.com>
2021-01-18 14:09:56 +0300

    feat: add webpack

---
+++ webpack.config.js
@@ -0,0 +1,41 @@
+const path = require('path');
+const { CleanWebpackPlugin } = require('clean-webpack-plugin');
+const CopyPlugin = require("copy-webpack-plugin");
+
+module.exports = {
+  entry: './src/index.ts',
+  mode: 'development',
+
+  devServer: {
+    contentBase: path.join(__dirname, 'build'),
+    port: 9000
+  },
+
+  module: {
+    rules: [
+      {
+        test: /\.tsx?$/,
+        use: 'ts-loader',
+        exclude: /node_modules/,
+      },
+    ],
+  },
+
+  plugins: [
+    new CleanWebpackPlugin(),
+    new CopyPlugin({
+      patterns: [
+        { from: "./src/public", to: "./" },
+      ],
+    }),
+  ],
+
+  resolve: {
+    extensions: [ '.tsx', '.ts', '.js' ],
+  },
+
+  output: {
+    filename: 'bundle.js',
+    path: path.resolve(__dirname, 'build'),
+  },
+};
