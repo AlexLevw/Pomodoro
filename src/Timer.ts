8d4689cefed50ff77ff2a21a40a90bf4820eccae
Aleksandr <foxi4554@gmail.com>
2021-01-20 21:56:41 +0300

    set start time

---
+++ src/Timer.ts
@@ -0,0 +1,57 @@
+export default class Timer {
+  timerInterval: NodeJS.Timeout;
+  timeCounter: number;
+  timerElement: Element;
+  startBtn: Element;
+  stopBtn: Element;
+  resetBtn: Element;
+
+  constructor() {
+    this.timerInterval = null;
+    this.timeCounter = 25 * 60 - 55;
+    this.timerElement = document.querySelector('.timer-numbers');
+    this.startBtn = document.querySelector('.start_btn');
+    this.stopBtn = document.querySelector('.stop_btn');
+    this.resetBtn = document.querySelector('.reset_btn');
+  }
+
+  public inti(): void {
+    this.startBtn.addEventListener('click', this.startTimer);
+    this.resetBtn.addEventListener('click', this.resetTimer);
+  }
+
+  private startTimer = (event: Event): void => {
+    event.target.removeEventListener('click', this.startTimer);
+    this.setTime();
+
+    this.timerInterval = setInterval(() => {
+      this.timeCounter -= 1;
+      this.setTime();
+    }, 1000);
+
+    this.stopBtn.addEventListener('click', this.stopTimer);
+  }
+
+  private stopTimer = (): void => {
+    this.stopBtn,removeEventListener('click', this.stopTimer);
+    clearInterval(this.timerInterval);
+    this.startBtn.addEventListener('click', this.startTimer);
+  }
+
+  private setTime = (): void => {
+    const secondsPerMinute = 60;
+    const minutes: number = Math.trunc(this.timeCounter / secondsPerMinute);
+    const seconds: number = Math.trunc(this.timeCounter % secondsPerMinute);
+    const formattedTime: string = `
+      ${ minutes < 10 ? `0` + minutes : minutes }:${ seconds < 10 ? `0` + seconds : seconds }
+    `;
+    document.title = formattedTime + ' - pomodoro';
+    this.timerElement.innerHTML = formattedTime;
+  }
+
+  private resetTimer = (): void => {
+    this.stopTimer();
+    this.timeCounter = 0;
+    this.setTime();
+  } 
+}
--- src/index.ts
+++ src/index.ts
@@ -1,60 +1,6 @@
-class Timer {
-  timerInterval: NodeJS.Timeout;
-  timeCounter: number;
-  timerElement: Element;
-  startBtn: Element;
-  stopBtn: Element;
-  resetBtn: Element;
-
-  constructor() {
-    this.timerInterval = null;
-    this.timeCounter = 0;
-    this.timerElement = document.querySelector('.timer-numbers');
-    this.startBtn = document.querySelector('.start_btn');
-    this.stopBtn = document.querySelector('.stop_btn');
-    this.resetBtn = document.querySelector('.reset_btn');
-  }
-
-  public intiTimer(): void {
-    this.startBtn.addEventListener('click', this.startTimer);
-    this.resetBtn.addEventListener('click', this.resetTimer);
-  }
-
-  private startTimer = (event: Event): void => {
-    event.target.removeEventListener('click', this.startTimer);
-    this.setTime();
-
-    this.timerInterval = setInterval(() => {
-      this.timeCounter += 1;
-      this.setTime();
-    }, 1000);
-
-    this.stopBtn.addEventListener('click', this.stopTimer);
-  }
-
-  private stopTimer = (): void => {
-    this.stopBtn,removeEventListener('click', this.stopTimer);
-    clearInterval(this.timerInterval);
-    this.startBtn.addEventListener('click', this.startTimer);
-  }
-
-  private setTime = (): void => {
-    const secondsPerMinute = 60;
-    const minutes: number = Math.trunc(this.timeCounter / secondsPerMinute);
-    const seconds: number = Math.trunc(this.timeCounter % secondsPerMinute);
-
-    this.timerElement.innerHTML = `
-    ${ minutes < 10 ? `0` + minutes : minutes }:${ seconds < 10 ? `0` + seconds : seconds }
-    `;
-  }
-
-  private resetTimer = (): void => {
-    this.stopTimer();
-    this.timeCounter = 0;
-    this.setTime();
-  } 
-}
+import './style.scss';
+import Timer from './Timer';
 
 const timer = new Timer();
 
-timer.intiTimer();
+timer.inti();
